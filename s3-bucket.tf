b28a2aa90a4440b191ce28f4237daed276f70443
Larry Ge <yanlei.ge@gmail.com>
2021-10-13 05:12:09 +0000

    update s3 bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -10,3 +10,15 @@ module "s3_bucket_for_logs" {
   attach_elb_log_delivery_policy = true  # Required for ALB logs
   attach_lb_log_delivery_policy  = true  # Required for ALB/NLB logs
 }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "lg-my-s3-bucket-test00912"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
